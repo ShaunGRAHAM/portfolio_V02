a2da356857f7cad06839958c1d81b752440fb6a5
Shaun Graham <shaun.o.graham@gmail.com>
2022-02-10 03:35:30 +0100

    fix font-size home

--- src/components/Navigation/navigationElements.js
+++ src/components/Navigation/navigationElements.js
@@ -21,12 +21,12 @@ export const NavbarContainer = styled.div`
 
 export const NavMenu = styled.a`
   text-decoration: none;
-  font-size: 16px;
+  font-size: 14px;
   color: black;
 `;
 
 export const ButtonLanguage = styled.button`
   position: relative;
   color: black;
-  font-size: 16px;
+  font-size: 14px;
 `;
